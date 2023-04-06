e3ef241b9aaad7d482c68ccc9c1e2bdf7447bb62
Wan  Hashim <whashim@gmail.com>
2023-04-06 11:23:09 +0800

    add testhello

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <template>
   <HelloWorld msg="Welcome OESB 2023"/>
+  <TestHello msg="Welcome OESB 2023"/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import TestHello from './components/Test.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,TestHello
   }
 }
 </script>
